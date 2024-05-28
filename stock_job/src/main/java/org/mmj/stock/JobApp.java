f51fcc20ef297d4738f01f14570813186af5dd4b
s7zr <755806250@qq.com>
2024-05-28 16:36:43 +0800

    JobApp启动类提交

---
+++ stock_job/src/main/java/org/mmj/stock/JobApp.java
@@ -0,0 +1,17 @@
+package org.mmj.stock;
+
+
+import org.mmj.stock.pojo.vo.TaskThreadPoolInfo;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@MapperScan("org.mmj.stock.mapper")//扫描mapper接口，生成代理对象，并被ioc容器管理
+@EnableConfigurationProperties({TaskThreadPoolInfo.class})
+public class JobApp {
+    public static void main(String[] args) {
+        SpringApplication.run(JobApp.class, args);
+    }
+}
